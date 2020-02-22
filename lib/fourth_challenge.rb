6313af27407846c4b4b4ae56e4b358f07320618e
Edward Horsey <edwardhorsey@me.com>
2020-02-22 23:47:02 +0000

    Automatically backed up by Learn

--- lib/fourth_challenge.rb
+++ lib/fourth_challenge.rb
@@ -7,12 +7,10 @@ def fourth_challenge
       :hero_friends => [
         {:name => "Benvolio"
           :age => "17"
-          :attitude => "worried"
-        },
+          :attitude => "worried"},
         {:name => "Mercuito"
           :age => "18"
-          :attitude => "hot-headed"
-        }
+          :attitude => "hot-headed"}
       ]
    }, 
    :capulet => {
@@ -22,12 +20,10 @@ def fourth_challenge
       :heroine_friends => [
         {:name => "Steven"
           :age => "30"
-          :attitude => "confused"
-        },
+          :attitude => "confused"},
         {:name => "Nurse"
           :age => "44"
-          :attitude => "worried"
-        }
+          :attitude => "worried"}
       ]
    }
 }
